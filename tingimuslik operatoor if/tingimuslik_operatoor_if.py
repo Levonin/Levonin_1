074e292bc7ecec1ae0781e90d6b6c056928fd39a
opilane <opilane@HP-CZC2349HT9>
2024-01-12 11:40:39 +0200

    kodutoo

--- tingimuslik operatoor if/tingimuslik_operatoor_if.py
+++ tingimuslik operatoor if/tingimuslik_operatoor_if.py
@@ -1,1 +1,21 @@
+nimi=input("mis sul nimi on?").capitalize()
+print("Tere,",nimi)
+if nimi=="Juku":
+    print("Lahme kinno")
+    vanus=int(input("Kui vana sa oled?"))
+    if vanus<0 or vanus>100:
+        pilet="vale vanus"
+    elif vanus<6:
+        pilet="tasuta pilet"
+    elif vanus<=14:
+        pilet="lastepilet"
+    elif vanus<=65:
+        pilet="täispilet"
+    elif vanus<=100:
+        pilet="sooduspilet"
+    print(pilet)    ### ilus vastu
+
+else:
+    print("Ma ootan Jukut")
+
 

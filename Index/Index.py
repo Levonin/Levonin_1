4b1d519c2f8aef24380038fdc23cd175b178d6b9
opilane <opilane@HP-CZC2349HT9>
2024-01-18 12:29:58 +0200

    Praktiline töö

---
+++ Index/Index.py
@@ -0,0 +1,15 @@
+﻿from http.client import UNPROCESSABLE_ENTITY
+
+
+print("Tere! olen sinu uus sõber - Python")
+nimi=input("Kirjuta sinu nimi:")
+print(nimi)
+print("Oi kui ilus nimi!")
+Vastus= int(input(nimi + ",Kas leian sinu keha indeksi? 0-ei,1-jah => "))
+if Vastus == 1:
+    pikkus=int(input("Kirjuta sinu pikkus: "))
+    mass = int(input("Kirjuta sinu mass: "))
+    indeks = mass /(0.01*pikkus)**2
+    print(nimi + "!Sinu keha indeks on: ",round(indeks))
+
+
